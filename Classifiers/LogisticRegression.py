434d67723f79d6c258bfb85e216ba67cf613f951
alessandro <alessandro.chiabodo@gmail.com>
2023-05-20 18:14:07 +0200

    refactor (LogisticRegression) : added the Classifier Interface

--- Classifiers/LogisticRegression.py
+++ Classifiers/LogisticRegression.py
@@ -3,13 +3,18 @@ import numpy
 import util
 import scipy.special
 
+from Classifiers.ClassifiersInterface import ClassifiersInterface
 
-class LogRegClass:
+
+class LogRegClass(ClassifiersInterface):
     def __init__(self, DTR: numpy.array, LTR: numpy.array, l=1e-3):
+        self.b = None
+        self.w = None
         self.DTR = DTR
         self.ZTR = LTR * 2.0 - 1
-        self.l = l
+        self.lam = l
         self.nSamples = DTR.shape[0]
+        self.trained = False
 
     def logreg_obj(self, v):
         # Compute and return the objective function value. You can retrieve all required information from self.DTR, self.LTR, self.l
@@ -17,18 +22,26 @@ class LogRegClass:
         b = v[-1]
         scores = numpy.dot(w.T, self.DTR) + b
         loss_per_sample = numpy.logaddexp(0, -self.ZTR * scores)
-        loss = 0.5 * self.l * numpy.linalg.norm(w) ** 2 + loss_per_sample.mean()
+        loss = 0.5 * self.lam * numpy.linalg.norm(w) ** 2 + loss_per_sample.mean()
         return loss
 
     def train(self):
         x0 = numpy.zeros(self.DTR.shape[0] + 1)
         xOpt, fOpt, d = scipy.optimize.fmin_l_bfgs_b(self.logreg_obj, x0=x0, approx_grad=True)
-        w, b = util.vcol(xOpt[0:self.DTR.shape[0]]), xOpt[-1]
-        return w, b
+        self.w, self.b = util.vcol(xOpt[0:self.DTR.shape[0]]), xOpt[-1]
+        self.trained = True
 
-    def evaluate(self, DTE: numpy.array, LTE: numpy.array):
-        w, b = self.train()
-        Score = numpy.dot(w.T, DTE) + b
+    def classify(self, DTE: numpy.array) -> numpy.array:
+        if not self.trained:
+            raise RuntimeError('Classifier is not trained yet')
+        Score = numpy.dot(self.w.T, DTE) + self.b
+        PLabel = (Score > 0).astype(int)
+        return PLabel
+
+    def evaluate(self, DTE: numpy.array, LTE: numpy.array) -> object:
+        if not self.trained:
+            raise RuntimeError('Classifier is not trained yet')
+        Score = numpy.dot(self.w.T, DTE) + self.b
         PLabel = (Score > 0).astype(int)
         Error = ((LTE != PLabel).astype(int).sum() / DTE.shape[1]) * 100
         return Error, PLabel
